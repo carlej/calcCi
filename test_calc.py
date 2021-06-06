81b41c6597bb0411427f17eb55ba6ce3ac6c68b3
carlej <carlej@oregonstate.edu>
2021-06-06 01:46:50 -0700

    name changes

--- test_calc.py
+++ test_calc.py
@@ -5,7 +5,7 @@ import calculator
 
 class TestCalculatorApp:
     def test_add(self):
-        assert calc.add(2,3) == 5
+        assert calculator.add(2,3) == 5
 
     def test_subtract(self):
-        assert calc.sub(8,3) == 5
+        assert calculator.sub(8,3) == 5
