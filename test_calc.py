482d4ab912218344499a3587fd3f352a0274268b
carlej <carlej@oregonstate.edu>
2021-06-06 01:52:46 -0700

    removed errors

--- test_calc.py
+++ test_calc.py
@@ -3,9 +3,11 @@ tests for calc app
 """
 import calculator
 
+
 class TestCalculatorApp:
     def test_add(self):
-        assert calculator.add(2,3) == 5
+        assert calculator.add(2, 3) == 5
+
 
     def test_subtract(self):
-        assert calculator.sub(8,3) == 5
+        assert calculator.sub(8, 3) == 5
