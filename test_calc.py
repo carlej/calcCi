5d2fc2cc4b9a4414cfa64eba4b4cd0ac698475eb
carlej <carlej@oregonstate.edu>
2021-06-06 01:59:51 -0700

    added new methods and tests

--- test_calc.py
+++ test_calc.py
@@ -10,3 +10,9 @@ class TestCalculatorApp:
 
     def test_subtract(self):
         assert calculator.sub(8, 3) == 5
+
+    def test_multiply(self):
+        assert calculator.mult(2, 3) == 6
+
+    def test_devide(self):
+        assert calculator.div(10, 5) == 2
